63ea8aa44473e8bd63a8ab4ef6940655f58a9785
dh-wsb <101039466+dh-wsb@users.noreply.github.com>
2022-04-03 19:28:32 +0200

    app changes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""docstring"""
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """docstring"""
     return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>'
-
 if __name__ == "__main__":
     app.run(debug=True)
